534aa29246a2a9a98b81b0ed5bc38406b2f477e4
SasiKiruthik <kiruthiksasi2408@gmail.com>
2026-01-13 20:30:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.use(express.json());
 
 app.use("/api/symptoms", symptomRoutes);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
